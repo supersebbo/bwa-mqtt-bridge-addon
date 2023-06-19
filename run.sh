40807d4ea48c475fb9faea97846e4581e04b43b0
Seb Belcher <sdjbelcher@gmail.com>
2023-06-19 12:58:16 +0100

    Added TCP config passthrough from HA

--- run.sh
+++ run.sh
@@ -9,6 +9,19 @@ MQTT_PASSWORD=$(bashio::services mqtt "password")
 # Concat the MQTT URI
 MQTT_URI="mqtt://${MQTT_USER}:${MQTT_PASSWORD}@${MQTT_HOST}"
 
+# Send the MQTT URI to the console for debugging - CAUTION: Password included
+echo $MQTT_URI
+
+# Grab the Bridge IP and Port from the Addon Config
+BRIDGE_IP=$(bashio::config "bridge_ip")
+BRIDGE_PORT=$(bashio::config "bridge_port")
+
+# Concat the TCP Connection String
+TCP_URI="tcp://${BRIDGE_IP}:${BRIDGE_PORT}/"
+
+# Send the TCP URI to the console for debugging
+echo $TCP_URI
+
 # Run the bwa-mqtt-bridge gem
 # TO DO Pull TCP settings from Addon config
-/usr/local/bundle/bin/bwa_mqtt_bridge ${MQTT_URI} tcp://10.10.10.214:9999/
+/usr/local/bundle/bin/bwa_mqtt_bridge ${MQTT_URI} ${TCP_URI}
